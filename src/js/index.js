fd18ced8401fcf6cc1bf7a2fb3bd7a96ca757594
Cristiano Epifânio <106474458+CRIS456ANO@users.noreply.github.com>
2022-08-04 16:51:41 -0300

    a interação funciona.

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,27 @@
 
 const avancar = document.getElementById("avancar");
+const voltar = document.getElementById("voltar");
+const modulo = document.querySelectorAll(".modulo");
 let moduloAtual = 0;
+ 
 avancar.addEventListener("click", function (){
-    const modulo = document.querySelectorAll(".modulo");
+    if(moduloAtual === modulo.length - 1) return;
+    const moduloSelecionado = document.querySelector(".selecionado");
+    moduloSelecionado.classList.remove("selecionado");
     moduloAtual++;
     console.log(moduloAtual)
     modulo[moduloAtual].classList.add("selecionado");
+   
+    
+});
+
+voltar.addEventListener("click", function (){
+    if(moduloAtual === 0) return;
     const moduloSelecionado = document.querySelector(".selecionado");
+    moduloSelecionado.classList.remove("selecionado");
+    moduloAtual--;
+    console.log(moduloAtual)
+    modulo[moduloAtual].classList.add("selecionado");
+    
     
 });
