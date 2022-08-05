10dd2d9b2fe1cb9c587d8e79c1d3eb91cefa9165
Cristiano Epifânio <106474458+CRIS456ANO@users.noreply.github.com>
2022-08-05 10:40:54 -0300

    adição de comentário no index.

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
-
+/* Constantes para a construção do slide de módulos da página inicial.*/
 const avancar = document.getElementById("avancar");
 const voltar = document.getElementById("voltar");
 const modulo = document.querySelectorAll(".modulo");
 let moduloAtual = 0;
  
+// Botão de avançar os slides na página principal.
 avancar.addEventListener("click", function (){
     if(moduloAtual === modulo.length - 1) return;
     const moduloSelecionado = document.querySelector(".selecionado");
@@ -11,10 +12,10 @@ avancar.addEventListener("click", function (){
     moduloAtual++;
     console.log(moduloAtual)
     modulo[moduloAtual].classList.add("selecionado");
-   
     
 });
 
+// Botão de voltar os slides na página principal.
 voltar.addEventListener("click", function (){
     if(moduloAtual === 0) return;
     const moduloSelecionado = document.querySelector(".selecionado");
