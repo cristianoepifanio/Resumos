a121e662a39369ddb0477c64601f270bfe87f228
Cristiano Epifânio <106474458+CRIS456ANO@users.noreply.github.com>
2022-08-07 20:20:24 -0300

    css ordem alfabetica

--- linguagens/src/js/index.js
+++ linguagens/src/js/index.js
@@ -5,7 +5,7 @@ const modulo = document.querySelectorAll(".modulo");
 const modulojs = document.querySelectorAll(".modulojs");
 let moduloAtual = 0;
  
-// Botão de avançar os slides na página principal.
+// Botão de avançar os slides na página sobre HTML e CSS.
 avancar.addEventListener("click", function (){
     if(moduloAtual === modulo.length - 1) return;
     const moduloSelecionado = document.querySelector(".selecionado");
@@ -16,7 +16,7 @@ avancar.addEventListener("click", function (){
     
 });
 
-// Botão de voltar os slides na página principal.
+// Botão de voltar os slides na página sobre HTML e CSS.
 voltar.addEventListener("click", function (){
     if(moduloAtual === 0) return;
     const moduloSelecionado = document.querySelector(".selecionado");
